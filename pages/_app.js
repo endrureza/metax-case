7010efe3c55e80539bed3fa13ff9ea20f6ba3b4a
Endru Reza <lotusb13@gmail.com>
2021-12-12 23:51:30 +0700

    chore: remove unnecessary file and add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import AuthContext from "@/context/AuthContext";
-import "../styles/globals.css";
+import 'nprogress/nprogress.css'
+import NProgress from "nprogress";
+import Router from "next/router";
+import { useEffect } from "react";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 800,
+  showSpinner: true,
+});
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => {
+      NProgress.start();
+    });
+
+    Router.events.on("routeChangeComplete", () => {
+      NProgress.done();
+    });
+
+    Router.events.on("routeChangeError", () => {
+      NProgress.done();
+    });
+  }, []);
 
   return (
     <AuthContext>
